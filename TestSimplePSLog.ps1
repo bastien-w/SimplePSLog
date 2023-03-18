5b1c775614f8587df55c8f734f493975540d0399
Bastien Werhann <bastien.werhann@outlook.com>
2023-03-18 11:50:16 +0100

    Added Verbose to test file

--- TestSimplePSLog.ps1
+++ TestSimplePSLog.ps1
@@ -1,13 +1,13 @@
 Import-Module .\SimplePSLog
 
-Start-Log -LogName $($MyInvocation.MyCommand.Name.Split(".")[0] + ".txt")
-#Start-Log -LogName $($MyInvocation.MyCommand.Name.Split(".")[0] + ".csv")
+Start-Log -LogName $($MyInvocation.MyCommand.Name.Split(".")[0] + ".txt") -Verbose
+#Start-Log -LogName $($MyInvocation.MyCommand.Name.Split(".")[0] + ".csv") -Verbose
 
-New-log -Type Information -Message "Script is run by user : $env:USERNAME"
-New-log -Type Information -Message "The script running is : $($MyInvocation.MyCommand.Name)"
-New-log -Type Information -Message "You can open the text file with excel and use the delimiter to get access to easier filtering"
+New-log -Type Information -Message "Script is run by user : $env:USERNAME" -Verbose
+New-log -Type Information -Message "The script running is : $($MyInvocation.MyCommand.Name)" -Verbose
+New-log -Type Information -Message "You can open the text file with excel and use the delimiter to get access to easier filtering" -Verbose
 
 
-New-log -Type Information -Message "This is an informationnal log"
-New-log -Type Warning -Message "This is a warning log"
-New-log -Type Error -Message "This is an error log"
+New-log -Type Information -Message "This is an informationnal log" -Verbose
+New-log -Type Warning -Message "This is a warning log" -Verbose
+New-log -Type Error -Message "This is an error log" -Verbose
