0a5c0f33ab3e05775e7b66b30cf8b57a2682bd6b
Bastien Werhann <bastien.werhann@outlook.com>
2023-04-05 11:10:28 +0200

    Added function information

--- SimplePSLog/SimplePSLog.psm1
+++ SimplePSLog/SimplePSLog.psm1
@@ -1,4 +1,21 @@
-# This function creates a log file with the specified path and name
+<#
+.SYNOPSIS
+    This function creates a log file with the specified path and name.
+
+.DESCRIPTION
+    The Start-Log function takes two optional parameters: $LogPath and $LogName. If the log file does not exist, it is created.
+
+.PARAMETER LogPath
+    The path to the directory where the log file will be created.
+
+.PARAMETER LogName
+    The name of the log file.
+
+.EXAMPLE
+    Start-Log -LogPath "C:\Logs" -LogName "MyLog.txt"
+
+    This example creates a log file named "MyLog.txt" in the "C:\Logs" directory.
+#>
 function Start-Log {
     [CmdletBinding()]
     param (
@@ -19,15 +36,42 @@ function Start-Log {
     }
 }
 
-# This function returns a timestamp in the format "dd/MM/yyyy ; HH:mm:ss ; "
+<#
+.SYNOPSIS
+    This function returns a timestamp in the format "dd/MM/yyyy ; HH:mm:ss ; ".
+
+.DESCRIPTION
+    The Set-Timestamp function takes no parameters and returns a timestamp in the format "dd/MM/yyyy ; HH:mm:ss ; ".
+
+.EXAMPLE
+    $timestamp = Set-Timestamp
+
+    This example sets the variable $timestamp to the current timestamp in the format "dd/MM/yyyy ; HH:mm:ss ; ".
+#>
 function Set-Timestamp {
     [CmdletBinding()]
     param()
     return Get-Date -Format "dd/MM/yyyy ; HH:mm:ss ; "
 }
 
-# This function writes messages to the log file with a timestamp and message type (Error, Warning, or Information)
-function New-log
+<#
+.SYNOPSIS
+    This function writes messages to the log file with a timestamp and message type (Error, Warning, or Information).
+
+.DESCRIPTION
+    The New-Log function takes two parameters: $Type and $Message. It writes messages to the log file with a timestamp and message type (Error, Warning, or Information).
+
+.PARAMETER Type
+    The type of message to write to the log file (Error, Warning, or Information).
+
+.PARAMETER Message
+    The message to write to the log file.
+
+.EXAMPLE
+    New-Log -Type "Error" -Message "An error has occurred"
+
+    This example writes an error message to the log file with a timestamp.
+#>function New-log
 {
     [CmdletBinding()]
      param (
